392617038d9dd029959fd5a9fddb8ddd71a8de25
Marco Bungart <marco.bungart@consol.de>
2020-11-18 11:04:29 +0100

    Added fault tolerance: retries

--- user/src/main/java/de/consol/dus/boundary/color/ColorRestClient.java
+++ user/src/main/java/de/consol/dus/boundary/color/ColorRestClient.java
@@ -1,6 +1,7 @@
 package de.consol.dus.boundary.color;
 
 import de.consol.dus.boundary.color.response.ColorResponse;
+import java.time.temporal.ChronoUnit;
 import java.util.Optional;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -9,6 +10,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.eclipse.microprofile.faulttolerance.Retry;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 @RegisterRestClient(configKey = "color-rest")
@@ -18,5 +20,6 @@ public interface ColorRestClient {
 
   @GET
   @Path("{name}")
+  @Retry(maxRetries = 10, delay = 1, delayUnit = ChronoUnit.SECONDS)
   Optional<ColorResponse> getColorByName(@PathParam("name") @NotNull @Size(max = 255) String name);
 }
