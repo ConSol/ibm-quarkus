868f48f212900399fff9de240f8c2cfc9b084cbe
Marco Bungart <marco.bungart@consol.de>
2020-11-18 11:19:09 +0100

    Added fallback

--- user/src/main/java/de/consol/dus/boundary/color/ColorRestClient.java
+++ user/src/main/java/de/consol/dus/boundary/color/ColorRestClient.java
@@ -10,6 +10,10 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import lombok.extern.slf4j.Slf4j;
+import org.eclipse.microprofile.faulttolerance.ExecutionContext;
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.FallbackHandler;
 import org.eclipse.microprofile.faulttolerance.Retry;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
@@ -21,5 +25,19 @@ public interface ColorRestClient {
   @GET
   @Path("{name}")
   @Retry(maxRetries = 10, delay = 1, delayUnit = ChronoUnit.SECONDS)
+  @Fallback(ColorFallback.class)
   Optional<ColorResponse> getColorByName(@PathParam("name") @NotNull @Size(max = 255) String name);
+
+  @Slf4j
+  final class ColorFallback implements FallbackHandler<Optional<ColorResponse>> {
+
+    @Override
+    public Optional<ColorResponse> handle(ExecutionContext executionContext) {
+      log.info("fallback called");
+      return Optional.of(ColorResponse.builder()
+          .hexdec("000000")
+          .name("blackForSadTimes")
+          .build());
+    }
+  }
 }
--- user/src/main/java/de/consol/dus/boundary/color/response/ColorResponse.java
+++ user/src/main/java/de/consol/dus/boundary/color/response/ColorResponse.java
@@ -1,8 +1,13 @@
 package de.consol.dus.boundary.color.response;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor
 @Data
 public class ColorResponse {
